b5351ede93467fcce00e755c33cbfe07f2ce3c00
amayas <amayas@mbp-de-amayas.home>
2023-11-21 15:17:44 +0100

    Auto-generated commit

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  API_URL: 'https://v3.football.api-sports.io',
-  API_KEY: '969efb4a4e1db5b41dac982cfe08a801'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  API_URL: 'https://v3.football.api-sports.io',
-  API_KEY: '969efb4a4e1db5b41dac982cfe08a801'
-};
