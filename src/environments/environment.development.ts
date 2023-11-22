aac302515a9459f627536bcfb14ae662b5e3c98e
mayes21 <amayas.abboute@live.fr>
2023-11-22 15:27:24 +0100

    update api key

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   API_URL: 'https://v3.football.api-sports.io',
-  API_KEY: 'fef233ae906f80eb1ba0bef32a9205b1'
+  API_KEY: '96e587cf50ad87ec5aa352b85f58e3dd'
 };
